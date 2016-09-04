c2b69be9d4497cdbcdc6c9e6f36b2574a6aa65a4
Steven Than <steventhan11@gmail.com>
2016-09-04 03:23:27 -0700

    done front page

--- deal_thief/views/default.py
+++ deal_thief/views/default.py
@@ -4,6 +4,9 @@ from datetime import datetime
 
 @view_config(route_name='home', renderer='../templates/home.html')
 def home_view(request):
+    slider = {
+                
+    }
     return {
         'page_title': 'Home',
         'today_date': datetime.now().strftime("%Y-%m-%d")
