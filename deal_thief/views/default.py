844e543d9f11340b16d3d3bec35197a6d357cfc1
Steven Than <steventhan11@gmail.com>
2016-09-03 19:49:02 -0700

    fix form display on mobile

--- deal_thief/views/default.py
+++ deal_thief/views/default.py
@@ -1,9 +1,10 @@
 from pyramid.view import view_config
 from ..models import User, Search
-
+from datetime import datetime
 
 @view_config(route_name='home', renderer='../templates/home.html')
 def home_view(request):
     return {
-        'page_title': 'Home' 
+        'page_title': 'Home',
+        'today_date': datetime.now().strftime("%Y-%m-%d")
     }
