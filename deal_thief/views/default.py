64d347f35edabe2918a2ce92655684ac89439f14
Victor Benavente <vbenavente@hotmail.com>
2016-09-05 15:02:09 -0700

    able to access request object from search_view method

--- deal_thief/views/default.py
+++ deal_thief/views/default.py
@@ -8,23 +8,6 @@ def home_view(request):
     slider = {
 
     }
-
-    location = checkin = checkout = error = ''
-    if request.method == "POST":
-        location = request.params.get('location', '')
-        checkin = request.params.get('checkin', '')
-        checkout = request.params.get('checkout', '')
-        if not location or not checkin or not checkout:
-            error = 'Location Checkin and Chckout are required'
-    if request.method == "POST":
-        import pdb; pdb.set_trace()
-        location = request.POST['location']
-        checkin = request.POST['start']
-        checkout = request.POST['end']
-        built = (location, checkin, checkout)
-        return HTTPFound(location='https://dealthief.herokuapp.com/search?' + built)
-    return {'error': error}
-
     return {
         'page_title': 'Home',
         'today_date': datetime.now().strftime("%Y-%m-%d")
@@ -41,4 +24,13 @@ def login_view(request):
 @view_config(route_name='search', renderer='../templates/search.html')
 def search_view(request):
     """Give us our search view."""
+    # location = checkin = checkout = error = ''
+    # if request.method == "POST":
+    #     location = request.params.get('location', '')
+    #     checkin = request.params.get('checkin', '')
+    #     checkout = request.params.get('checkout', '')
+    #     if not location or not checkin or not checkout:
+    #         error = 'Location Checkin and Chckout are required'
+    import pdb; pdb.set_trace()
+    built = [request.params['location'], request.params['start'], request.params['end']]
     return {}
